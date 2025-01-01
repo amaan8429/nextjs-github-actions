7c963594118dbdedf3d8090a469a47a1c156021a
amaan8429 <mohdamaan8429@gmail.com>
2025-01-01 12:06:24 +0530

    unsued var removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
             className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[
         -name:var(--font-geist-sans)]"
         >
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start   ">
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <Image
                     className="dark:invert          "
                     src="/next.svg"
